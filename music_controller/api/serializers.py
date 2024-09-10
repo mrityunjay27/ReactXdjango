c5cff3623242f6eb81f9cccfe356656308221c81
mrityunjay27 <mrityunjay27062000@gmail.com>
2024-09-11 00:23:47 +0530

    Created room model and its view and serialize

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,10 @@
+# Serialzer to translate python code (models) to JSON object.
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 
+                  'votes_to_skip', 'created_at')
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,10 @@
 # Urls local to this app 'api'
 
 from django.urls import path
-from .views import main
+from .views import main, RoomView
 
 
 urlpatterns = [
     path('home', main),
+    path('room', RoomView.as_view())
 ]
